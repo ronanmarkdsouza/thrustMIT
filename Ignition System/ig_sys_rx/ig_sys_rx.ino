8e25cb6e33a22791afcf10216ed068ff3be726bb
darpan-t007 <81548501+darpan-t007@users.noreply.github.com>
2021-10-29 09:37:41 +0530

    Receiver with nrf24L01

--- Ignition System/ig_sys_rx/ig_sys_rx.ino
+++ Ignition System/ig_sys_rx/ig_sys_rx.ino
@@ -1,1 +1,48 @@
-
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+RF24 radio(8, 10); // CE, CSN
+char check1='f';
+int led_counter=0;
+const byte addresses [][6] = {"00001", "00002"};
+int relay = 4;
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(addresses[0]);      //Setting the address at which we will send the data
+  radio.openReadingPipe(1, addresses[1]);
+  radio.setPALevel(RF24_PA_MIN);
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, HIGH);
+}
+void loop() 
+{
+  delay(5);
+  radio.startListening();
+  
+  if (radio.available())
+  {
+    int text;
+    radio.read(&text, sizeof(text));
+    if (text == 1)
+    {
+      led_counter=1;
+      Serial.println("Armed");
+    }
+    else if (text == 2)
+    {
+      Serial.println("Launched");
+      digitalWrite(relay, LOW);
+      delay(5000);
+      digitalWrite(relay, HIGH);
+    }
+  }
+  
+  delay(5);
+  radio.stopListening(); 
+  
+   if(led_counter==0)
+  {
+  radio.write(&check1,sizeof(check1));
+  }
+}
